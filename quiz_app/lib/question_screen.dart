ff9342ed490634ca6506c68a90a3ac7eceba9bce
mortadaal <mortada001@hotmail.com>
2023-09-12 08:17:47 +0200

    added data folder and model

--- quiz_app/lib/question_screen.dart
+++ quiz_app/lib/question_screen.dart
@@ -1,17 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/styled_text.dart';
+import 'package:quiz_app/data/questions.dart';
 
-class QuestionScreen extends StatefulWidget{
+class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
   @override
-  State<QuestionScreen> createState(){
+  State<QuestionScreen> createState() {
     return _QuestionScreenState();
   }
-
 }
 
-class _QuestionScreenState extends State<QuestionScreen>{
+class _QuestionScreenState extends State<QuestionScreen> {
   @override
-  Widget build(context){
-    return const Text('This is QuestionScreen');
+  Widget build(context) {
+    final currentQuestion= questions[0];
+    return  SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          StayledText(currentQuestion.text),
+         const SizedBox(height: 30),
+        AnswerButton(answerText:questions,onTap: (){}),
+        AnswerButton(answerText:'Answer 2',onTap: (){}),
+        AnswerButton(answerText:'Answer 3',onTap: (){}),
+        ],
+      ),
+    );
   }
 }
